a7daecb7ee76455da897f41ee4b363c7a3f4389d
roni shu <ronishulman53@gmail.com>
2024-07-18 16:30:29 +0300

    tweadked app for heroku

--- shift/forms.py
+++ shift/forms.py
@@ -62,7 +62,7 @@ class ShiftUpdateForm(ModelForm):
    
     class Meta:
         model = Shift
-        fields = ['shift_start_date_time', 'shift_end_date_time', 'type_of_shift', 'client', 'location', 'amount_of_km', 'public_transport', 'food', 'parking_refund']
+        fields = ['type_of_shift', 'client', 'shift_start_date_time', 'shift_end_date_time', 'location', 'amount_of_km', 'public_transport', 'food', 'parking_refund']
 
         labels = {
             'client': 'לקוח',
