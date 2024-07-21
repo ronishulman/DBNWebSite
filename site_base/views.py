6551910a3f7d89c1375e354f4183086ad7ac9ce7
roni shu <ronishulman53@gmail.com>
2024-07-21 18:12:44 +0300

    Changes have been made

--- site_base/views.py
+++ site_base/views.py
@@ -145,12 +145,14 @@ def clients_info(request):
 
 @login_required
 def employee_details(request, id):
+    print("im in employee details fun")
     user = request.user
     required_employee = Employee.objects.get(id = id)
     connected_user_shifts = Shift.objects.filter(employee_id = request.user.id)
     calculate_employees_details(required_employee)
 
     if request.method == "POST":
+        print("im in the if")
         _month = request.POST.get('month')
         _year = request.POST.get('year')
         
@@ -163,7 +165,7 @@ def employee_details(request, id):
             user_shifts = Shift.objects.filter(employee_id = id, shift_start_date_time__month =_month, shift_start_date_time__year =_year ).order_by("-shift_id")
             context = {'required_employee': required_employee, 'data':user_shifts, 'user': user}
             return render(request,"site_base/employeedetails.html",context) 
-
+    return render(request,"site_base/employeedetails.html",{'required_employee': required_employee, 'data':connected_user_shifts, 'user': user}) 
 @login_required
 def client_details(request):
      user = request.user
