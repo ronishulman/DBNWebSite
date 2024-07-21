9c3949669fcb0909734915898031645e186136bd
roni shu <ronishulman53@gmail.com>
2024-07-21 15:14:11 +0300

    Changes have been made

--- employee/admin.py
+++ employee/admin.py
@@ -1,5 +1,7 @@
 # admin.py
 from django.contrib import admin
 from .models import Employee
+from .models import EmployeeMonthlyData
 
 admin.site.register(Employee)
+admin.site.register(EmployeeMonthlyData)
