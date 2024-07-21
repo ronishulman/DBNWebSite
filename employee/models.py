6e4cf18b34dd7244eadc3ebfaa517231f283fa1a
roni shu <ronishulman53@gmail.com>
2024-07-21 15:12:19 +0300

    Changes have been made

--- employee/models.py
+++ employee/models.py
@@ -20,5 +20,14 @@ class Employee(models.Model):
     shift_start_date_time = models.DateTimeField(null=True)
     shift_end_date_time = models.DateTimeField(null=True)
 
+class EmployeeMonthlyData(models.Model):
+    employee = models.ForeignKey(Employee, on_delete=models.CASCADE)
+    month = models.DateField()
+    salary = models.FloatField()
+    total_km = models.FloatField()
+    total_transport = models.FloatField()
+    total_food = models.FloatField()
+    total_parking = models.FloatField()
+
     def __str__(self):
         return f'{self.first_name} {self.last_name}'
