5f16085f6443a942ed2c0308d9435c8718649ba7
roni shu <ronishulman53@gmail.com>
2024-07-22 14:46:23 +0300

    Changes have been made

--- employee/admin.py
+++ employee/admin.py
@@ -3,5 +3,24 @@ from django.contrib import admin
 from .models import Employee
 from .models import EmployeeMonthlyData
 
+
+class EmployeeMonthlyDataAdmin(admin.ModelAdmin):
+    list_display = ('employee_first_name', 'employee_last_name', 'employee_id', 'month', 'salary', 'total_km', 'total_transport', 'total_food', 'total_parking')
+
+    def employee_first_name(self, obj):
+        return obj.employee.first_name
+
+    def employee_last_name(self, obj):
+        return obj.employee.last_name
+
+    def employee_id(self, obj):
+        return obj.employee.id
+
+    employee_first_name.short_description = 'First Name'
+    employee_last_name.short_description = 'Last Name'
+    employee_id.short_description = 'ID'
+
+
+admin.site.register(EmployeeMonthlyData, EmployeeMonthlyDataAdmin)
+
 admin.site.register(Employee)
-admin.site.register(EmployeeMonthlyData)
