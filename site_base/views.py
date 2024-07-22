7c616080810ecda0444db19ad7b6e520b16a46e6
roni shu <ronishulman53@gmail.com>
2024-07-22 15:49:09 +0300

    Changes have been made

--- site_base/views.py
+++ site_base/views.py
@@ -384,7 +384,10 @@ def aggregate_monthly_data(request):
     return HttpResponse('Invalid request method.', status=405)
 
 def Employee_Monthly_Data_Detail(request,id):
-    employee_monthly_data = get_object_or_404(EmployeeMonthlyData, id=id)
+    employee = get_object_or_404(Employee, id=id)
+
+    employee_monthly_data = get_object_or_404(EmployeeMonthlyData, employee=employee)
+    
     return render(request, 'employee_monthly_data_detail.html', {
             'employee_monthly_data': employee_monthly_data
         })
