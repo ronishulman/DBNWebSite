f3dce32b5b3134ec75f6d0bd7a1fec9df7d185bc
roni shu <ronishulman53@gmail.com>
2024-07-29 13:00:14 +0300

    made changes

---
+++ site_base/management/commands/clear_old_messages.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from site_base.models import UpdateMessages 
+
+class Command(BaseCommand):
+    help = 'Clear old update messages that are older than a week.'
+
+    def handle(self, *args, **options):
+        today = timezone.now().date()
+
+        if today.weekday() == 6: 
+            one_week_ago = timezone.now() - timezone.timedelta(weeks=1)
+            UpdateMessages.objects.filter(created_at__lt=one_week_ago).delete()
+            self.stdout.write(self.style.SUCCESS(f'Successfully deleted last week update messages.'))
